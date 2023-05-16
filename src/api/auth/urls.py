6a3f17773c56fe34b6b8e9d850a7941b27a0a267
danielmiranda0099 <danielmesa877@gmail.com>
2023-05-16 12:58:56 -0500

    [ADD] Custom Token Claims

--- src/api/auth/urls.py
+++ src/api/auth/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -8,6 +7,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
